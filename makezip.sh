229faf41719c1992773d9156d71c156d8bc06ff7
4nm1tsu <4nm1tsu@4nm1tsu.com>
2026-01-31 22:27:37 +0900

    build: include git tag in zip filename

--- makezip.sh
+++ makezip.sh
@@ -1,1 +1,3 @@
-zip -r ../radical-cobblemon-trainers-ja.zip pack.mcmeta assets data -x "*.git*" -x "makezip.sh" -x "trainers_original" -x "localize.py" -x "title_map.json"
+TAG=$(git describe --tags --abbrev=0)
+echo "Using tag: $TAG"
+zip -r "../radical-cobblemon-trainers-ja$TAG.zip" pack.mcmeta assets data -x "*.git*" -x "makezip.sh" -x "trainers_original" -x "localize.py" -x "title_map.json"
