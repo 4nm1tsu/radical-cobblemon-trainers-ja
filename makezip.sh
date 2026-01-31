14bd3b26356666d17e9f44f8496bf8b70fb49203
4nm1tsu <4nm1tsu@4nm1tsu.com>
2026-01-31 21:15:57 +0900

    chore(build): exclude dev files from zip

--- makezip.sh
+++ makezip.sh
@@ -1,1 +1,1 @@
-zip -r ../rct-japanese-pack.zip pack.mcmeta assets data -x "*.git*" -x "makezip.sh"
+zip -r ../rct-japanese-pack.zip pack.mcmeta assets data -x "*.git*" -x "makezip.sh" -x "trainers_original" -x "localize.py" -x "title_map.json"
