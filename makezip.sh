9171152c19b18faae73acef65104cc369e4887e8
4nm1tsu <4nm1tsu@4nm1tsu.com>
2026-02-06 00:19:12 +0900

    build: fix zip filename tag separator

--- makezip.sh
+++ makezip.sh
@@ -1,3 +1,3 @@
 TAG=$(git describe --tags --abbrev=0)
 echo "Using tag: $TAG"
-zip -r "../radical-cobblemon-trainers-ja$TAG.zip" pack.mcmeta assets data -x "*.git*" -x "makezip.sh" -x "trainers_original" -x "localize.py" -x "title_map.json"
+zip -r "../radical-cobblemon-trainers-ja-$TAG.zip" pack.mcmeta assets data -x "*.git*" -x "makezip.sh" -x "trainers_original" -x "localize.py" -x "title_map.json"
